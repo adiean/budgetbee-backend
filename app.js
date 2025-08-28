a7fc8f392fe378d0fd2f84adc3fd7e7e14e5bc75
Tanya <12hellotanya@gmail.com>
2025-08-28 23:56:16 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const userRouter = require("./routes/userRouter");
+const mongoose = require("mongoose");
+const errorHandler = require("./middlewares/errorHandlerMiddleware");
+const categoryRouter = require("./routes/categoryRouter");
+const transactionRouter = require("./routes/tansactionRouter");
+const cors = require("cors");
+const app = express();
+const URI = "mongodb+srv://<db_username>:<db_password>@budgetbee.geixrkv.mongodb.net/?retryWrites=true&w=majority&appName=budgetbee";
+
+//! Connect to mongoDb
+mongoose
+  .connect(URI)
+  .then(() => console.log("connected to db"))
+  .catch((e) => console.log(e));
+
+//!Cors Configuratin
+const corsOptions = { origin: ["http://localhost:5173", "https://budgetbees.vercel.app"] };
+app.use(cors(corsOptions));
+
+//!Middlewares
+app.use(express.json());
+//!Routes
+app.use("/", userRouter);
+app.use("/", categoryRouter);
+app.use("/", transactionRouter);
+
+//!Error
+app.use(errorHandler);
+//!Start the server
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
